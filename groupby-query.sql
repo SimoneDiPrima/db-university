30ed6615746167b4481ed31e3a30d54298fdc0f5
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-06 17:07:27 +0200

    es2

--- groupby-query.sql
+++ groupby-query.sql
@@ -5,5 +5,13 @@ FROM `students`
 GROUP BY `enrolment year`;
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(*) as 'Teacher`offices', `office_address` as 'offices address'
+FROM `teachers`
+GROUP BY `offices address`;
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
+
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
