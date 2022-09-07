a2d7024cf329b44ef3fc151c978eae5c31e11e7a
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-07 16:10:03 +0200

    es3

--- join.sql
+++ join.sql
@@ -16,8 +16,20 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
 
 -- -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT `teachers`.`name`,
+`teachers`.`surname`,
+`course_teacher`.`course_id`,
+`courses`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id`= `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
+
 -- -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- -- relativo dipartimento, in ordine alfabetico per cognome e nome
 -- -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
 -- -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
