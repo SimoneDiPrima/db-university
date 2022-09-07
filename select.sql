b99ed4b16385f0eaa2034ae5d91a72801575d618
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-07 20:48:59 +0200

    modifiche es3

--- select.sql
+++ select.sql
@@ -50,7 +50,7 @@ WHERE `level` = 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 
-SELECT *
+SELECT COUNT(*) as "University 's departments"
  FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
