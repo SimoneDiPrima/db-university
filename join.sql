3d01add35e46ccb13824268ada4c816941718974
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-07 15:43:58 +0200

    es2

--- join.sql
+++ join.sql
@@ -8,6 +8,13 @@ ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 -- -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `departments`.`name` as "Departments 's Name",
+`degrees`.`name` as "Course's Name"
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
 -- -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 -- -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- -- relativo dipartimento, in ordine alfabetico per cognome e nome
