3437366a96284d63502659c34a72c16ae4522319
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-07 16:18:34 +0200

    es5

--- join.sql
+++ join.sql
@@ -30,6 +30,15 @@ WHERE `teachers`.`id` = 44;
 -- -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- -- relativo dipartimento, in ordine alfabetico per cognome e nome
 -- -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `teachers`.`name`,
+`teachers`.`surname`,
+`course_teacher`.`course_id`,
+`courses`.`name`
+FROM `teachers`
+JOIN `course_teacher`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id`= `course_teacher`.`teacher_id`
+WHERE `teachers`.`id` = 44;
 -- -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 -- -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
